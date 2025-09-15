7721c414fc428c754d4c9ed5e44badb0540bb8ff
Sudais Ahmad Khan <sudaisahmadkhan8@gmail.com>
2025-09-15 11:51:11 +0500

    some changes has been made

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let headings = ["SMART IT SOLUTIONS FOR YOUR BUSINESS GROWTH", "ENGAGING CONTENT THAT BUILDS TRUST & DRIVES RESULTS", "POWERFUL WEBSITES DESIGNED TO PERFORM & SCALE", "INTUITIVE DESIGNS CRAFTED FOR SEAMLESS USER EXPERIENCES"];
+let headings = [`SMART IT SOLUTIONS FOR YOUR <span class="text-secondary">BUSINESS GROWTH</span>`, `ENGAGING CONTENT THAT BUILDS TRUST & <span class="text-secondary">DRIVES RESULTS</span>`, `POWERFUL WEBSITES DESIGNED TO <span class="text-secondary">PERFORM & SCALE</span>`, `INTUITIVE DESIGNS CRAFTED FOR SEAMLESS <span class="text-secondary">USER EXPERIENCES</span>`];
 let background = [
     "linear-gradient(94.2deg, #021E2D 47.66%, #021e2d80 96.58%), url('./images/bg.jpg')",
     "linear-gradient(94.2deg, #021E2D 47.66%, #021e2d80 96.58%), url('./images/download.jpeg')",
     "linear-gradient(94.2deg, #021E2D 47.66%, #021e2d80 96.58%), url('./images/In hd quality________.jpeg')",
     "linear-gradient(94.2deg, #021E2D 47.66%, #021e2d80 96.58%), url(\"./images/Marvel's Spider-Man HD Spider Symbol Wallpaper.jpeg\")"
-  ];
+];
 let container = document.querySelector(".pagination");
 let children = container.children;
 let heroHeading = document.querySelector("#heading");
@@ -21,7 +21,7 @@ let widths = Array.from(children).map(el => el.offsetWidth + gap);
 let index = 0;
 let idx = 0;
 let move = 0;
-let time = 3000;
+let time = 4000;
 
 function slider() {
         setInterval(() => {
@@ -32,14 +32,16 @@ function slider() {
             index = 0;
             time = 0;
             container.style.transition = "none"; 
+            container.style.transition = "none"; 
             container.style.transform = `translateX(0px)`;
             children[0].classList.add("text-secondary");
-            heroHeading.innerText = headings[0];
+            heroHeading.innerHTML = headings[0];
             heroSection.style.background = background[0];
             heroSection.style.backgroundRepeat = "no-repeat";
             heroSection.style.backgroundSize = "cover"
             setTimeout(() => {
-                container.style.transition = "transform 0.3s";
+                container.style.transition = "transform 0.5s ease-in-out";
+                children.style.transition = "all 0.5s ease-in-out";
             }, 50);
         } else {
             container.style.transform = `translateX(-${move}px)`;
@@ -55,7 +57,7 @@ function slider() {
             }else {
                 idx = 0;
             }
-            heroHeading.innerText = headings[idx];
+            heroHeading.innerHTML = headings[idx];
             heroSection.style.background = background[idx];
             heroSection.style.backgroundRepeat = "no-repeat";
             heroSection.style.backgroundSize = "cover"
